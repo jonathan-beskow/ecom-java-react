f30b7c01b83d5dec81197f8198b694fbe2fe7345
M373746 <j.beskow@sistran.com.br>
2026-08-20 09:11:14 -0300

    feat: creating a demo project to deep dive in sprin security

---
+++ security/src/main/java/com/security/demo/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.security.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        //httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+
+}
