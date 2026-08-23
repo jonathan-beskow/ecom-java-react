721a55e15961435f82aa4aea2759ff6eadba0240
M373746 <j.beskow@sistran.com.br>
2026-08-23 10:11:17 -0300

    feat: implementing and improving relationship between tables

---
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/model/Role.java
@@ -0,0 +1,25 @@
+package com.ecommerce.sb_ecom.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, name = "role_name")
+    private AppRole roleName;
+
+
+}
