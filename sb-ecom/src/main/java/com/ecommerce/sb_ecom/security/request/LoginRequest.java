32a8aa47f85dff2f962ea87136e8d643cb5b768e
M373746 <j.beskow@sistran.com.br>
2026-08-26 08:51:13 -0300

    feat: implementing more security and starting controller

--- sb-ecom/src/main/java/com/ecommerce/sb_ecom/security/request/LoginRequest.java
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/security/request/LoginRequest.java
@@ -1,8 +1,13 @@
-package com.ecommerce.sb_ecom.security.jwt;
+package com.ecommerce.sb_ecom.security.request;
+
+import jakarta.validation.constraints.NotBlank;
 
 public class LoginRequest {
+
+    @NotBlank
     private String username;
 
+    @NotBlank
     private String password;
 
     public String getUsername() {
